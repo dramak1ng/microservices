6c01b24abb3ceec093893ddb109b563718c9dd29
dramak1ng <kensi11qw@gmail.com>
2025-03-08 18:48:44 +0300

    complete task 3.1.2

---
+++ src/main/java/ru/itmentor/spring/boot_security/demo/dto/UserDto.java
@@ -0,0 +1,19 @@
+package ru.itmentor.spring.boot_security.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    private String username;
+
+    private String lastName;
+
+    private String password;
+}
